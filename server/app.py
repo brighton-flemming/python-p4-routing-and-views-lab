54b79e21cfd5ca2120c4a66ddc29ee760c535cb2
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-19 19:54:44 +0300

    Mission Passed

--- server/app.py
+++ server/app.py
@@ -4,5 +4,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route('/<string:string>')
+def print_string(string):
+    print f"{string}"
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
